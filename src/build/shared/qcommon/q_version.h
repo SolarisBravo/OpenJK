fb6a3c83d4d88456c14e9cfeab59169c25d5f37c
SolarisBravo <elitkramer@gmail.com>
2021-04-02 11:08:12 -0700

    Initial Commit

---
+++ src/build/shared/qcommon/q_version.h
@@ -0,0 +1,7 @@
+/* #undef SOURCE_DATE */
+#define GIT_HASH "d2ed03a016f54917bc1dcf06bcc59d3f674fb533"
+/* #undef BUILD_PORTABLE */
+
+#if !defined(SOURCE_DATE)
+#  define SOURCE_DATE __DATE__
+#endif
